5940fceb29bc9c3c51aea9cddc7347439f55b266
hassan308 <hassanfarahh308@gmail.com>
2024-11-02 01:47:28 +0100

    te

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,35 @@
 import { Toaster } from 'react-hot-toast'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { auth } from '../app/firebase/firebaseConfig';
+import { isSignInWithEmailLink, signInWithEmailLink } from 'firebase/auth';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isSignInWithEmailLink(auth, window.location.href)) {
+      let email = window.localStorage.getItem('emailForSignIn');
+      if (!email) {
+        email = window.prompt('Ange din e-postadress för bekräftelse');
+      }
+      signInWithEmailLink(auth, email, window.location.href)
+        .then(() => {
+          window.localStorage.removeItem('emailForSignIn');
+          router.push('/'); // Omdirigera till startsidan eller önskad sida efter inloggning
+        })
+        .catch((error) => {
+          console.error('Error signing in with email link', error);
+        });
+    }
+  }, [router]);
+
   return (
     <>
       <Component {...pageProps} />
       <Toaster />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
